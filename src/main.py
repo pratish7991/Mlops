e06e42f06be0e9771959e384ad7eb08c911d7b6a
Pratish Dewangan <dpratishraj7991@gmail.com>
2026-03-02 08:02:52 +0530

    added preprocessing into main.py

--- src/main.py
+++ src/main.py
@@ -6,10 +6,27 @@ from src.models.multimodal_model import MultiModalModel
 from src.training.trainer import Trainer
 from src.utils.seed import set_seed
 from src.utils.logger import setup_logger
+import os
+from src.preprocessing.preprocess import preprocess_data
 
 
 @hydra.main(config_path="../configs", config_name="config", version_base=None)
 def main(cfg: DictConfig):
+
+
+
+    data_path = cfg.data.preprocessed_path
+
+    if not os.path.exists(data_path):
+        print("Processed dataset not found. Running preprocessing...")
+        preprocess_data(
+            base_path=cfg.data.base_path,
+            num_people=cfg.data.num_people,
+            output_path=data_path,
+        )
+
+    dataset = PreprocessedDataset(data_path)
+
 
     print("Configuration:\n", OmegaConf.to_yaml(cfg))
 
